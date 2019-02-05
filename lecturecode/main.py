f579aea77da6f8ed78e916e83fdb15012a5ddd21
Harrison Hayes <harrisonayes97@yahoo.com>
2019-02-05 14:18:43 -0600

    added some code we did in lecture

---
+++ lecturecode/main.py
@@ -0,0 +1,23 @@
+# x = 12
+
+# def foo():
+#     global x
+#     print (x)
+#     x = 10
+#     print (x)
+
+
+# foo()
+# print (x)
+
+
+a = 2
+
+def foo():
+    b = a + 1
+    print(c)
+
+
+# foo()
+c = 95
+foo()
