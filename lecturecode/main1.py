c6e762d6bbac8bd7f8d57871fc17c2d9b68413ee
Harrison <harrisonhayes97@yahoo.com>
2019-02-13 07:55:19 -0600

    added some stuff

---
+++ lecturecode/main1.py
@@ -0,0 +1,39 @@
+# x = 12
+
+# def foo():
+#     global x
+#     print (x)
+#     x = 10
+#     print (x)
+
+
+# foo()
+# print (x)
+
+
+#a = 2
+
+#def foo():
+#    b = a + 1
+#    print(c)
+
+
+# foo()
+#c = 95
+#foo()
+
+##################### 2/12/2019
+
+#def A(I, P):
+#    def B():
+#        print (I)
+#    if I >= 1:
+#        B()
+#    else:
+#        A(2, B)
+#def C():
+#    pass
+#
+#A(1, C)
+
+
